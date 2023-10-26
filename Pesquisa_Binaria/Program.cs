12dd5e9e251573bcc03d013ca510737e421e6686
Klleriston <klleandrade@Outlook.com>
2023-10-26 11:01:50 -0300

    Adicionar arquivos de projeto.

---
+++ Pesquisa_Binaria/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] listaBinaria = new int[5] { 1, 2, 3, 4, 5 };
+
+static void PesquisaBinaria(int[] listaBinaria, int item)
+{
+    int baixo = 0;
+    int alto = listaBinaria.Length - 1;
+
+    while (baixo <= alto) 
+    {
+        int meioDaLista = (baixo + alto) / 2; 
+        int chute = listaBinaria[meioDaLista];
+        
+        if (chute == item) 
+        {
+            Console.WriteLine($"O numero é {chute}");
+        } 
+        if (chute > item) 
+        {
+            alto = meioDaLista - 1;
+        }
+        else
+        {
+            baixo = meioDaLista + 1;
+        }
+    }
+    return;
+}
+
+PesquisaBinaria(listaBinaria, 3);
